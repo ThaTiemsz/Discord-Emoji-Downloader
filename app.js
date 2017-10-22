8e26e19e6c0dd0e225e37bef3b7cd3a8c91df5c0
Tiemen <Tiemen2@live.nl>
2017-10-22 15:42:16 +0200

    Rename conflicted emojis
    Fixes #1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Emoji = emojiID => `https://cdn.discordapp.com/emojis/${emojiID}.png`;
 const $downloadBtn = $(`<button class="ui labeled icon red button" id="download"><i class="cloud icon"></i>Download</button>`);
 
+
 $(document).ready(() => {
 	$("#submit").click(async () => {
 		const code = editor.getSession().getValue();
@@ -11,15 +12,16 @@ $(document).ready(() => {
 			if (!guild.emojis) return error("I couldn't find the emojis object.");
 			if (guild.emojis.length < 1) return error("This server doesn't have any emojis!");
 			const cleanGuildName = guild.name.replace(/\s/g, "_").replace(/\W/g, "");
-			console.log(guild.emojis.length);
+			console.log("Emojis:", guild.emojis.length);
 
 			show("#loading");
 
+			const renamedEmoji = renameEmoji(guild.emojis);
 			const zip = new JSZip();
 			let count = 0;
-			for (i in guild.emojis) {
-				let req = await fetch("https://cors-anywhere.herokuapp.com/" + Emoji(guild.emojis[i].id), { method: "get", mode: "cors"	});
-				zip.file(`${guild.emojis[i].name}.png`, req.arrayBuffer());
+			for (let i in renamedEmoji) {
+				let req = await fetch("https://cors-anywhere.herokuapp.com/" + Emoji(renamedEmoji[i].id), { method: "get", mode: "cors"	});
+				zip.file(`${renamedEmoji[i].name}.png`, req.arrayBuffer());
 				count++;
 			}
 
@@ -45,5 +47,38 @@ $(document).ready(() => {
 	const error = (message) => {
 		$("#error-msg").text(message);
 		show("#error");
+	}
+
+	function renameEmoji(emojis) {
+		if (!emojis) return console.error("No Emojis Array");
+		const emojiCountByName = {};
+		this.disambiguatedEmoji = [];
+		this.customEmojis = {};
+		this.emojisByName = {};
+		this.emojisById = {};
+
+		const disambiguateEmoji = emoji => {
+			const originalName = emoji.name;
+			const existingCount = emojiCountByName[originalName] || 0;
+			emojiCountByName[originalName] = existingCount + 1;
+			if (existingCount > 0) {
+				const name = `${originalName}~${existingCount}`;
+				emoji = {
+					...emoji,
+					name,
+					originalName
+				};
+			}
+
+			this.emojisByName[emoji.name] = emoji;
+			if (emoji.id) {
+				this.emojisById[emoji.id] = emoji;
+				this.customEmojis[emoji.name] = emoji;
+			}
+			this.disambiguatedEmoji.push(emoji);
+		};
+
+		emojis.forEach(disambiguateEmoji);
+		return disambiguatedEmoji;
 	}
 });
