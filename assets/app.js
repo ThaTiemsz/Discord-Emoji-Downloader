91de7f45d9dbce54b42737f2489cbc81542ce209
Tiemen <Tiemen2@live.nl>
2019-01-09 03:28:00 +0100

    v1.1 * Doesn't use a CORS proxy anymore

---
+++ assets/app.js
@@ -0,0 +1,238 @@
+require("@babel/polyfill");
+const request = require("request-promise-native");
+const JSZip = require("jszip");
+const { saveAs } = require("file-saver");
+const $ = require("./jquery-3.2.1.min.js");
+window.jQuery = $;
+
+const $downloadBtn = $(`<button class="ui labeled icon red button" id="download"><i class="cloud icon"></i>Download</button>`);
+const Emoji = (emojiID, animated = false) => `https://cdn.discordapp.com/emojis/${emojiID}.${animated ? "gif" : "png"}?v=1`;
+const API = {
+    host: "https://discordapp.com/api/v6",
+    emojis: (guild) => `/guilds/${guild}/emojis`,
+    guilds: "/users/@me/guilds",
+    guild: (id) => `/guilds/${id}`,
+    request: (method, endpoint, token) => {
+        return request(API.host + endpoint, {
+            method,
+            headers: {
+                "Authorization": token
+            },
+            json: true,
+            resolveWithFullResponse: true
+        });
+    }
+}
+const sortAlpha = (a, b) => {
+    a = a.name.toLowerCase();
+    b = b.name.toLowerCase();
+    return a < b ? -1 : a > b ? 1 : 0
+}
+
+$(document).ready(function() {
+    $(".menu .item").tab();
+    $("#emojis").hide();
+
+    $("#tokenHelp").click(() => {
+        $('.ui.basic.modal').modal('show');
+    });
+
+    this.guild = [];
+    this.emojis = [];
+    $("#default-1 #continue").click(async (e) => {
+        e.preventDefault(e);
+
+        let success;
+        let token = $("#token").val();
+        $("#continue").addClass("loading");
+
+        if (!token) return;
+        token = token.replace(/^"(.+)"$/, "$1");
+
+        success = true
+
+        let res;
+        try {
+            res = await API.request("GET", API.guilds, token);
+        } catch ({ statusCode }) {
+            return error(statusCode === 401 ? "Invalid token." : "Could not authenticate with Discord.");
+        }
+
+        const guilds = res.body.sort(sortAlpha);
+        const guildsDropdown = guilds.map(guild => {
+            return {
+                name: guild.icon
+                    ? `<img src="https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png" />${guild.name}`
+                    : guild.name,
+                value: guild.id
+            }
+        });
+
+        $("#server-select").dropdown({
+            values: guildsDropdown,
+            placeholder: "Select Server",
+            onChange: async (value, text, $selected) => {
+                $("#default-2").append(`<div class="ui active dimmer"><div class="ui loader"></div></div>`);
+                let res;
+                try {
+                    res = await API.request("GET", API.guild(value), token);
+                } catch {
+                    return error("Could not fetch server emojis.");
+                }
+
+                const guild = res.body;
+                this.guild = guild;
+                this.emojis = guild.emojis;
+                this.emojis = renameEmoji(this.emojis);
+                this.emojis = this.emojis.sort(sortAlpha);
+                const emojis = this.emojis;
+
+                let emojisDropdown = [];
+                for (const emoji in emojis) {
+                    emojisDropdown.push({
+                        name: `<img src="${Emoji(emojis[emoji].id, emojis[emoji].animated)}" style="width: 1.5em!important; height: 1.5em!important;" /> ${emojis[emoji].name}`,
+                        value: emojis[emoji].id,
+                        selected: true
+                    });
+                }
+
+                $("#emoji-select").dropdown({
+                    values: emojisDropdown,
+                    placeholder: "Select Emojis",
+                    onChange: (value, text, $selected) => {
+                        $("#emojicount").text(`(${$("input[name='emojis']").val().split(",").length}/${emojis.length})`);
+                    }
+                })
+
+                $("#emojis").show();
+                $(".active.dimmer").remove();
+            }
+        });
+
+        $("#default-1").attr("data-tab", "default-hide");
+        $("#default-2").attr("data-tab", "default");
+        $.tab("change tab", "default");
+    });
+
+    $("#default-2 #submit").click(async (e) => {
+        e.preventDefault(e);
+
+        if (!this.emojis.length) return error("Please select at least one emoji.");
+        try {
+            if (this.guild.emojis.length < 1) return error("This server doesn't have any emojis!");
+            const cleanGuildName = this.guild.name.replace(/\s/g, "_").replace(/\W/g, "");
+            console.log("Emojis:", this.emojis.length);
+
+            show("#loading");
+
+            const renamedEmoji = renameEmoji(this.emojis);
+            const zip = new JSZip();
+            let count = 0;
+            for (let i in renamedEmoji) {
+                const req = await request(Emoji(renamedEmoji[i].id, renamedEmoji[i].animated), { resolve: null });
+                zip.file(`${renamedEmoji[i].name}.${renamedEmoji[i].animated ? "gif" : "png"}`, req);
+                count++;
+            }
+
+            $("#success-msg strong").text(count);
+            show("#success");
+            $("#default-2 #submit").after($downloadBtn);
+
+            $("#download").click(() => {
+                zip.generateAsync({ type: "blob" }).then(content => {
+                    saveAs(content, `Emojis_${cleanGuildName}.zip`);
+                });
+            })
+        } catch(err) {
+            return error(err);
+        }
+    });
+
+    $("#manual #submit").click(async (e) => {
+        e.preventDefault(e);
+
+        const code = editor.getSession().getValue();
+        if (!code) return error("You should probably get some code in there.");
+        try {
+            const guild = JSON.parse(code);
+            if (!guild.id) return error("Your code seems off... are you sure you pasted the guild object?");
+            if (!guild.emojis) return error("I couldn't find the emojis object.");
+            if (guild.emojis.length < 1) return error("This server doesn't have any emojis!");
+            const cleanGuildName = guild.name.replace(/\s/g, "_").replace(/\W/g, "");
+            console.log("Emojis:", guild.emojis.length);
+
+            show("#loading");
+
+            const renamedEmoji = renameEmoji(guild.emojis);
+            const zip = new JSZip();
+            let count = 0;
+            for (let i in renamedEmoji) {
+                const req = await request(Emoji(renamedEmoji[i].id, renamedEmoji[i].animated), { resolve: null });
+                zip.file(`${renamedEmoji[i].name}.png`, req);
+                count++;
+            }
+
+            $("#success-msg strong").text(count);
+            show("#success");
+            $("#manual #submit").after($downloadBtn);
+
+            $("#download").click(() => {
+                zip.generateAsync({ type: "blob" }).then(content => {
+                    saveAs(content, `Emojis_${cleanGuildName}.zip`);
+                });
+            })
+        } catch(err) {
+            return error("Recheck your code, it threw some syntax errors.", err);
+        }
+    });
+
+
+    $("button#continue").click(() => {
+        $("#error").hide();
+    });
+
+    function show(id) {
+        $("#messages div.message").hide();
+        $(id).fadeIn("slow").css("display", "inline-flex");
+    }
+
+    function error(message, ...args) {
+        console.error(message, ...args);
+        $("button").removeClass("loading");
+        $("#error-msg").text(message);
+        show("#error");
+    }
+
+    function renameEmoji(emojis) {
+        if (!emojis) return console.error("No Emojis Array");
+        const emojiCountByName = {};
+        const disambiguatedEmoji = [];
+        const customEmojis = {};
+        const emojisByName = {};
+        const emojisById = {};
+
+        const disambiguateEmoji = emoji => {
+            const originalName = emoji.name;
+            const existingCount = emojiCountByName[originalName] || 0;
+            emojiCountByName[originalName] = existingCount + 1;
+            if (existingCount > 0) {
+                const name = `${originalName}~${existingCount}`;
+                emoji = {
+                    ...emoji,
+                    name,
+                    originalName
+                };
+            }
+
+            emojisByName[emoji.name] = emoji;
+            if (emoji.id) {
+                emojisById[emoji.id] = emoji;
+                customEmojis[emoji.name] = emoji;
+            }
+            disambiguatedEmoji.push(emoji);
+        };
+
+        emojis.forEach(disambiguateEmoji);
+        return disambiguatedEmoji;
+    }
+});
